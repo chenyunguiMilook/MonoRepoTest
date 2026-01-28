4e94fc8a2acbf883300ef102e5c239d03ebf89d9
chenyungui <linxitj2003@icloud.com>
2026-01-28 09:42:00 +0800

    initial commit

---
+++ Packages/ModuleA/Sources/ModuleA/People.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class People {
+    public var name: String
+    
+    public init(name: String) {
+        self.name = name
+    }
+}
---
+++ Packages/ModuleB/Sources/ModuleB/ModuleB.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import ModuleA
+
+public class Family {
+    
+    public let peoples: [People]
+    
+    public init(peoples: [People]) {
+        self.peoples = peoples
+    }
+}
